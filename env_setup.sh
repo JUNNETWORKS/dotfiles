bc34540294be6ed16d6fff1f98d93f2968b66ae2
Junichi Tanaka <junichi200123@gmail.com>
2020-11-04 15:49:29 +0900

    first commit

---
+++ env_setup.sh
@@ -0,0 +1,8 @@
+# zsh (require "oh-my-zsh")
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# neovim
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim ~/.vim
+mkdir -p ~/.config
+ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
