adb7fe2f768d2be16f22a188a79e9b99e9a90577
Junichi Tanaka <junichi200123@gmail.com>
2020-11-04 16:14:15 +0900

    tmux関連の設定を追加

--- env_setup.sh
+++ env_setup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # zsh (require "oh-my-zsh")
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
@@ -6,3 +7,10 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 mkdir -p ~/.config
 ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
+
+# tmux
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.tmux.powerline.conf ~/.tmux.powerline.conf
+
+# scripts
+ln -sf ~/dotfiles/scripts ~/scripts
